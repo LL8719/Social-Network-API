b83c34eb4b13fef511d8a3b0049722c0b92708b5
LL8719 <arsgatslr_87@yahoo.com>
2023-04-25 18:13:58 -0700

    set up thought and reactions model

--- models/thought.js
+++ models/thought.js
@@ -1,3 +1,32 @@
 const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema({});
+const thoughtSchema = new Schema({
+	thoughtText: {
+		type: String,
+		required: true,
+		minLength: 1,
+		maxLength: 280,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	reactions: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'reaction',
+		},
+	],
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+	return this.reactions.length;
+});
+
+const thought = model('thought', thoughtSchema);
+
+module.exports = thought;
